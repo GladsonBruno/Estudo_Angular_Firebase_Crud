f11211a06e973c365be9f29907b562632db13d6a
= <=>
2017-10-16 18:03:37 -0200

    Adicionando firebase ao projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+
+export const firebaseconfig = {
+    apiKey: "AIzaSyDgb8OmZXnGuNLvYgAILt_zzP2mkNRQhFk",
+    authDomain: "estudoangularfirebase.firebaseapp.com",
+    databaseURL: "https://estudoangularfirebase.firebaseio.com",
+    projectId: "estudoangularfirebase",
+    storageBucket: "estudoangularfirebase.appspot.com",
+    messagingSenderId: "508065140556"
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseconfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
